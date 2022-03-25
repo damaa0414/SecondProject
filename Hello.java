002866f858436c8035f07405eb95028f68439eb2
damaa0414 <damaa0414@gmail.com>
2022-03-25 14:49:19 +0900

    add name

--- Hello.java
+++ Hello.java
@@ -4,8 +4,9 @@ public class Hello {
 		System.out.println("login");
                 System.out.println("logoff");
 		System.out.println("logtwo");
-		System.out.println("what is Git?")
+		System.out.println("what is Git?");
 		System.out.println("That's DVCD");
+		System.out.println("Boyoun");
 
 	}
 
